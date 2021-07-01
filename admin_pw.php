64b4387765786087323c2be90359f7c1bc326390
harshalkardile <69854015+harshalkardile@users.noreply.github.com>
2021-07-01 19:43:43 +0530

    Add files via upload

---
+++ admin_pw.php
@@ -0,0 +1,39 @@
+<html>
+<body bgcolor="skyblue">
+<center><pre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</pre>
+<?php
+//$user="harshal";
+//$pass="root123";
+$i=$_POST['i'];
+$p=$_POST['p'];
+
+
+//if($user==$i && $pass==$p)
+if($i=='harshal' && $p=='root123')
+   header("Location: http://localhost/gym/admin.html");
+
+else
+   echo "Please Enter Correct User id and Password";
+?>
+<br>
+<a href="admin_pw.html">go back</a>
+
+</body>
+</html>
