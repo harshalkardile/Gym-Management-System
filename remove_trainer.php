5c5cbe62020c37ae8fced270c4ecf260c6c21e60
harshalkardile <69854015+harshalkardile@users.noreply.github.com>
2021-07-18 10:30:26 +0530

    Add files via upload

---
+++ remove_trainer.php
@@ -0,0 +1,29 @@
+<?php
+$r=$_POST['r'];
+
+
+$con=pg_connect("host=localhost user=harshal password=root123 dbname=gym") or die ("Unable to connect database");
+
+$qry="select * from trainer where t_id=$r";
+
+$rs=pg_query($con,$qry) or die ("Can't perform operations on datebase,check your query no1**"); //1st QUERY**
+
+$cnt=pg_num_rows($rs);
+
+if($cnt==0)
+{
+        die("no record find for this tid $r !!");
+}
+
+$qry="delete from trainer where t_id=$r";
+
+pg_query($con,$qry) or die ("Problem to Delete query");   //2nd QUERY**
+
+pg_close($con);
+
+echo "Data of Trainer ID:$r deleted Sucessfully !!";
+
+
+?>
+<a href="remove_trainer.html">Go Back</a>
+              
