369be41666f1255194b12166ebb49011f6c308da
harshalkardile <69854015+harshalkardile@users.noreply.github.com>
2021-07-21 14:10:05 +0530

    Add files via upload

---
+++ update_trainer.php
@@ -0,0 +1,28 @@
+<?php 
+$r=$_POST['r'];
+$p=$_POST['p'];
+
+$con=pg_connect("host=localhost user=harshal password=root123 dbname=gym") or die ("Unable to connect database");
+
+$qry="select * from trainer where t_id=$r";
+
+$rs=pg_query($con,$qry) or die ("Can't perform operations on datebase,check your query no1**"); //1st QUERY**
+
+$cnt=pg_num_rows($rs);
+
+if($cnt==0)
+{
+	die("no record find for this rno $r !!");
+}
+
+$qry="update trainer set t_shift='$p' where t_id=$r";
+
+pg_query($con,$qry) or die ("Problem to update query");   //2nd QUERY**
+
+pg_close($con);
+
+echo "updated Sucessfully !!!<br>";
+
+
+?>
+<a href="update_trainer.html">Go Back</a>
