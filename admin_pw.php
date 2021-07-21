47940107e1df3cc19db734d280d304dd60c60f96
harshalkardile <69854015+harshalkardile@users.noreply.github.com>
2021-07-21 14:05:57 +0530

    Update admin_pw.php

--- admin_pw.php
+++ admin_pw.php
@@ -12,8 +12,7 @@ $pass="root123";
 if($user==$i && $pass==$p)
 //if($i=='harshal' && $p=='root123')
    header("Location: http://localhost/gym/admin.html");
-else
-   echo "Please Enter Correct User id and Password";
+
 ?>
 <br>
 <a href="admin_pw.html">go back</a>
