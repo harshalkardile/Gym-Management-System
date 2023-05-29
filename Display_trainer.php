a9ab1c6fb7244991559ca29da28bb7f86f4bb75f
harshalkardile <69854015+harshalkardile@users.noreply.github.com>
2023-05-29 10:41:10 +0530

    Update Display_trainer.php

--- Display_trainer.php
+++ Display_trainer.php
@@ -87,7 +87,6 @@ pg_close($con);
         </div>
 
         <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-
         <script src="vendor/bootstrap/js/popper.js"></script>
         <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
